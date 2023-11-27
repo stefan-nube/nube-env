aac370acc69d30e85045183ed5a0ef61f472da3e
Stefan <stefan@wearenube.xyz>
2023-11-27 10:01:26 +0100

    .

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
 import './styles/style.css'
 
 console.log('Hello this works!')
-
-createBadge()
-animateTitle()
